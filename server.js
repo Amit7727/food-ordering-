fba12adfee5a70f508eb10b53e2e7855a9fe4c66
Amit kumar jangir <amitjangid223322@gmail.com>
2021-08-21 17:43:51 +0530

    add auth

--- server.js
+++ server.js
@@ -8,16 +8,32 @@ const PORT = 3000;
 
 app.use(express.static('public'))
 
-app.get("/",(req,res)=>
-{
-    res.render('home');
-})
+
 
 //set tamplet engine....
 app.use(expresslayout);
 app.set('views',path.join(__dirname,'/resources/views'));
 app.set('view engine','ejs');
 
+app.get("/",(req,res)=>
+{
+    res.render('home');
+})
+
+app.get("/cart",(req,res)=>
+{
+    res.render("customers/cart");
+})
+
+app.get("/login",(req,res)=>
+{
+    res.render("auth/login");
+})
+
+app.get("/register",(req,res)=>
+{
+    res.render("auth/register");
+})
 
 
 app.listen(PORT,()=>
